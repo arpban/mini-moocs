ec58eeb3b5b69622730283ef4410a5b6e8caf2ec
ArpitX <arpit.bansal2010@gmail.com>
2017-07-22 16:48:32 +0530

    initial

---
+++ src/js/main.js
@@ -0,0 +1,15 @@
+function openDrawer(){
+    $('.drawer-handle').toggleClass('open');
+    $('.dropdown-menu').toggleClass('open');
+    $('header').toggleClass('open');
+}
+
+$(window).on("scroll", function() {
+    if($(window).scrollTop() > 50) {
+        $(".navbar").addClass("active");
+    } else {
+        //remove the background property so it comes transparent again (defined in your css)
+       $(".navbar").removeClass("active");
+    }
+});
+
